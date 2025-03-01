f6d9111c4b299e4a9bdae38c76d4181fed845fd1
Prasad3916 <pambiprasad4@gmail.com>
2025-03-01 17:51:50 +0530

    Added

---
+++ JAVA TASK 10KC/9.BLC AND ELC/com/Blc/StudentMain.java
@@ -0,0 +1,15 @@
+package com.Blc;
+
+public class StudentMain {
+
+	public static void main(String[] args) {
+		Student stu1=Student.getStudentDetails(01,39,"Bhavani Prasad");
+		System.out.println(stu1.getId());
+		System.out.println(stu1.getRollno());
+		System.out.println(stu1.getName());
+		Student stu2=Student.getStudentDetails(01,39,"Pambi Bhavani Prasad");
+		System.out.println(stu2.getId());
+		System.out.println(stu2.getRollno());
+		System.out.println(stu2.getName()); 
+	}
+}
