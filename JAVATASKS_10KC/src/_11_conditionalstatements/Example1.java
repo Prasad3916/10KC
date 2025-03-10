40289fad7dbfcacc4aea718a26e173ddcbf55e83
Prasad3916 <pambiprasad4@gmail.com>
2025-03-10 14:46:49 +0530

    Java Task Added

---
+++ JAVATASKS_10KC/src/_11_conditionalstatements/Example1.java
@@ -0,0 +1,19 @@
+package _11_conditionalstatements;
+
+public class Example1 {
+	
+	
+	boolean isEven(int num) {
+		if(num%2==0) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+	public static void main(String args[]) {
+		Example1 e=new Example1();
+		boolean res=e.isEven(100);
+		System.out.println(res);
+	}
+}
---
+++ JAVATASKS_10KC/src/_11_conditionalstatements/Example2.java
@@ -0,0 +1,27 @@
+package _11_conditionalstatements;
+
+public class Example2 {
+	String checkNumber(int num) {
+		if(num>0) {
+			return "Positive";
+		}
+		else {
+			if(num==0) {
+				return "Zero";
+			}
+			else {
+				return "Negative";
+			}
+		}
+	}
+	public static void main(String[] args) {
+		Example2 e2=new Example2();
+		String res1=e2.checkNumber(10);
+		String res2=e2.checkNumber(-10);
+		String res3=e2.checkNumber(0);
+		System.out.println(res1);
+		System.out.println(res2);
+		System.out.println(res3);
+
+	}
+}
